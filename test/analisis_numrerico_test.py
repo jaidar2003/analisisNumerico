c154d1f82943cfe391e43e1986797a96b796a910
Juanma <j.aidar@alumno.um.edu.ar>
2024-04-06 13:16:57 -0300

    FIX

--- test/analisis_numrerico_test.py
+++ test/analisis_numrerico_test.py
@@ -17,3 +17,12 @@ class TestMinimosCuadrados:
         assert mc.x == [1, 2, 3, 4, 5]
         assert mc.y == [2, 3, 4, 5, 6]
 
+    def test_ajuste_lineal_fail(self):
+        mc = MinimosCuadrados()
+        mc.ajuste_lineal()
+        assert mc.x is None
+        assert mc.y is None
+        assert mc.x == []
+        assert mc.y == []
+
+    
